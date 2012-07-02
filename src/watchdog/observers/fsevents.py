464c1ea97809013fe5c9347df3d6b080ba273121
Travis McLane <tmclane@mezeo.com>
2012-07-02 16:10:57 -0500

    report directory events before file events

--- src/watchdog/observers/fsevents.py
+++ src/watchdog/observers/fsevents.py
@@ -86,16 +86,6 @@ if platform.is_darwin():
         events = new_snapshot - self.snapshot
         self.snapshot = new_snapshot
 
-        # Files.
-        for src_path in events.files_deleted:
-          self.queue_event(FileDeletedEvent(src_path))
-        for src_path in events.files_modified:
-          self.queue_event(FileModifiedEvent(src_path))
-        for src_path in events.files_created:
-          self.queue_event(FileCreatedEvent(src_path))
-        for src_path, dest_path in events.files_moved:
-          self.queue_event(FileMovedEvent(src_path, dest_path))
-
         # Directories.
         for src_path in events.dirs_deleted:
           self.queue_event(DirDeletedEvent(src_path))
@@ -105,6 +95,16 @@ if platform.is_darwin():
           self.queue_event(DirCreatedEvent(src_path))
         for src_path, dest_path in events.dirs_moved:
           self.queue_event(DirMovedEvent(src_path, dest_path))
+
+        # Files.
+        for src_path in events.files_deleted:
+          self.queue_event(FileDeletedEvent(src_path))
+        for src_path in events.files_modified:
+          self.queue_event(FileModifiedEvent(src_path))
+        for src_path in events.files_created:
+          self.queue_event(FileCreatedEvent(src_path))
+        for src_path, dest_path in events.files_moved:
+          self.queue_event(FileMovedEvent(src_path, dest_path))
 
 
     def run(self):
